4b6e48be0fa6363df5aad149be0369ea4eb73cd4
neilbartlett <neil.e.bartlett@gamil.com>
2024-01-25 22:45:13 +0000

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, middleware};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
@@ -7,8 +7,13 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| {
-        App::new().service(greet)
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(greet)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
